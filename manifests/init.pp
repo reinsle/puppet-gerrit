b9b5931ea51c4a08ed69fb71cc7b3bd1a0e05492
Robert Einsle <robert@einsle.de>
2012-10-14 19:38:10 +0200

    Adding Management for gerrit User / Group

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@
 # Standard class parameters
 # Define the general class behaviour and customizations
 #
-# [*gerrit_version*]
+    # [*gerrit_version*]
 #   Version of gerrit
 #
 # == Author
@@ -16,16 +16,27 @@
 #
 class gerrit (
     $gerrit_version       = params_lookup('gerrit_version'),
+    $gerrit_group         = params_lookup('gerrit_group'),
+    $gerrit_gid           = params_lookup('gerrit_gid'),
     $gerrit_user          = params_lookup('gerrit_user'),
     $gerrit_home          = params_lookup('gerrit_home'),
+    $gerrit_uid           = params_lookup('gerrit_uid'),
     $gerrit_database_type = params_lookup('gerrit_database_type')
     ) inherits gerrit::params {
 
+    # Crate Group for gerrit
+    group { $gerrit_group:
+        gid    => "$gerrit_gid", 
+        ensure => "present",
+    }
+
     # Create User for gerrit-home
     user { $gerrit_user:
-        comment => "$email",
-        home    => "$gerrit_home",
-        shell   => "/bin/false",
+        comment    => "User for gerrit instance",
+        home       => "$gerrit_home",
+        shell      => "/bin/false",
+        uid        => "$gerrit_uid",
+        gid        => "$gerrit_gid",
         ensure     => "present",
         managehome => true,
     }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,14 @@ class gerrit::params {
   $gerrit_version = $::operatingsystem ? {
     default => '2.4.2',
   }
+  # Group which gerrit is in
+  $gerrit_group = $::operatingsystem ? {
+    default => 'gerrit',
+  }
+  # GroupId of gerrit group
+  $gerrit_gid = $::operatingsystem ? {
+    default => '515',
+  }
   # Username, gerrit runs on
   $gerrit_user = $::operatingsystem ? {
     default => 'gerrit',
@@ -11,6 +19,10 @@ class gerrit::params {
   # Home-Directory where gerrit whould be installed
   $gerrit_home = $::operatingsystem ? {
     default => '/opt/gerrit',
+  }
+  # UserID of created gerrit User
+  $gerrit_uid = $::operatingsystem ? {
+    default => '515',
   }
   # type of Database storing configs of gerrit ['mysql' / 'pgsql']
   $gerrit_database_type = $::operatingsystem ? {
