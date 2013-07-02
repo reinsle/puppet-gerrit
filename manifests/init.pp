b15eac3e29a3a9a593236bf17c9254d72f590348
David Schmitt <david@dasz.at>
2013-07-02 16:55:37 +0200

    add configurable heap memory limit
    Originally from 46bde2100f7@edlerd/puppet-gerrit by David Edler <de@plista.com>
    Adapted and squashed.

--- manifests/init.pp
+++ manifests/init.pp
@@ -58,6 +58,7 @@ class gerrit (
   $gerrit_database_type = params_lookup('gerrit_database_type'),
   $gerrit_java          = params_lookup('gerrit_java'),
   $gerrit_java_home     = params_lookup('gerrit_java_home'),
+  $gerrit_heap_limit    = params_lookup('gerrit_heap_limit'),
   $canonical_web_url    = params_lookup('canonical_web_url'),
   $sshd_listen_address  = params_lookup('sshd_listen_address'),
   $httpd_listen_url     = params_lookup('httpd_listen_url'),
